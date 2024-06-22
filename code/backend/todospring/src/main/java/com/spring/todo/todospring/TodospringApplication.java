b6991f813476c246c9490339f35e8d7782a6dbbe
Mauricio Barbosa <mauriciobarboza6.1@gmail.com>
2024-06-22 12:30:18 -0300

    first commit

---
+++ code/backend/todospring/src/main/java/com/spring/todo/todospring/TodospringApplication.java
@@ -0,0 +1,14 @@
+package com.spring.todo.todospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodospringApplication.class, args);
+	}
+
+}
+ 
---
+++ code/backend/todospring/src/test/java/com/spring/todo/todospring/TodospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.todo.todospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
